5f2bf19be40b16bd4bd7ada2eab43eb8e45d201a
t-kobayashi-da <t_kobayashi@dreamarts.co.jp>
2025-11-09 13:09:16 +0900

    add: react-admin & json-server

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # test-react-admin
+
+
+```bash
+cd frontend
+npm ci
+```
+
+```bash
+cd test-react-admin/frontend
+npm run json-server
+```
+
+```bash
+cd test-react-admin/frontend
+npm run dev
+```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://localhost:4000');
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
+
+export default App;
